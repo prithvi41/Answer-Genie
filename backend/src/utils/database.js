30bfaa7eb5ea2beefe392e8ec61991cd9544e161
Prithvi41 <rajp7163@gmail.com>
2024-07-09 15:52:54 +0530

    Clouddb connection

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -3,28 +3,17 @@ require('dotenv').config();
 const path = require('path');
 const fs = require('fs');
 
-const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-});
+var conString = process.env.API 
+var client = new Client(conString);
 
 client.connect((err) => {
     if (err) {
         console.log(err);
-        return new Error("Unable to connect to the database");
+    } 
+    else {
+        console.log("Database connected");
     }
-    const schemaPath = path.join(__dirname, '../models/schema.sql');
-    const schema = fs.readFileSync(schemaPath, 'utf-8');
-    client.query(schema, (err) => {
-        if (err) {
-            console.log(err);
-            return new Error("unexpected error");
-        }
-        console.log("schema created successfully");
-    })
-});
+}
+);
 
 module.exports = client;
